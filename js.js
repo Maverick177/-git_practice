61d8fa6987c68080495030b8c3cb015c7e5c15a8
Maverick177 <belegotimauricio@gmai.com>
2022-05-24 11:09:13 -0300

    project codeacademy added to new branch on git_practice repository

---
+++ js.js
@@ -0,0 +1,29 @@
+let projectstitle = document.getElementsByClassName('dropdown1')[0];
+let dropdown = document.getElementsByClassName('bottom-left')[0];
+let menudrop = () =>{
+  dropdown.classList.toggle('bottomblock');
+}
+projectstitle.onmouseover = menudrop;
+
+
+let projectstitle2 = document.getElementsByClassName('dropdown2')[0];
+let dropdown2 = document.getElementsByClassName('bottom-left2')[0];
+let menudrop2 = () =>{
+  dropdown2.classList.toggle('bottomblock2');
+}
+projectstitle2.onmouseover = menudrop2;
+
+
+let projectstitle3 = document.getElementsByClassName('dropdown3')[0];
+let dropdown3 = document.getElementsByClassName('bottom-left3')[0];
+let menudrop3 = () =>{
+  dropdown3.classList.toggle('bottomblock3');
+}
+projectstitle3.onmouseover = menudrop3;
+
+let projectstitle4 = document.getElementsByClassName('dropdown4')[0];
+let dropdown4 = document.getElementsByClassName('bottom-left4')[0];
+let menudrop4 = () =>{
+  dropdown4.classList.toggle('bottomblock4');
+}
+projectstitle4.onmouseover = menudrop4;
